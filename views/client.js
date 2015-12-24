8bf4452a55ba4119c3c17aacad5177b5d758c3f0
Alex Bennett <alexandergraybennett@gmail.com>
2015-12-24 15:48:01 -0700

    iterating over response obj

--- views/client.js
+++ views/client.js
@@ -1,19 +1,30 @@
 $(document).ready(function(){
-  function getTaskData(){
-    $.get( "/get", function( data ) {
-      return new Promise(function(resolve, reject){
-        console.log(data);
-      })
-    });
-  } getTaskData().then(function(data){
-    formatTaskData();
+  getTaskData().then(function(data){
+    formatTaskData(data);
+  }).catch(function(error){
+    console.error("Couldn't get task data", error);
   });
-})
+});
 
-function formatTaskData(data){
-  console.log(data)
-}
+  function getTaskData(){
+    return new Promise(function(resolve, reject){
+      $.get('/get', function(data){
+        resolve(data);
+      });
+    });
+  };
 
+  function formatTaskData(data){
+    return new Promise(function(resolve, reject){
+      console.log('this is your data ', data)
+      for(var i = 0; i < data.tasks.length; i = i + 1){
+        var task = data.tasks[i];
+        console.log(task);
+      }
+    });
+  };
+
+////////////////
 //
 // function getNewTaskData(form){
 //     var formValues = form.serializeArray();
