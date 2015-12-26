c663b9e0a1405701cb3d8bd612ad8e49ab4150ec
Alex Bennett <alexandergraybennett@gmail.com>
2015-12-26 12:50:52 -0700

    delete button

--- views/client.js
+++ views/client.js
@@ -36,26 +36,29 @@ function printTasks(tasks){
       var taskPoints = item.points;
       var taskComplete = item.complete;
 
-      var taskItemContainer = document.createElement("p");
+      var taskCompleteButton = document.createElement('button');
+      var taskDeleteButton = document.createElement('button');
+      var deleteButtonText = document.createTextNode("Delete task");
+      var taskItemContainer = document.createElement("ul");
       var taskName = document.createTextNode(item.task);
       var taskPoints = document.createTextNode(item.points);
       var taskComplete = document.createTextNode(item.complete);
 
+      taskDeleteButton.appendChild(deleteButtonText);
       taskItemContainer.appendChild(taskName);
       taskItemContainer.appendChild(taskPoints);
       taskItemContainer.appendChild(taskComplete);
+      taskItemContainer.appendChild(taskCompleteButton);
+      taskItemContainer.appendChild(taskDeleteButton);
 
       var taskListItem = document.createElement("li");
       taskListItem.appendChild(taskItemContainer);
       $(".task-list").append(taskItemContainer);
-
     })
     resolve(tasks);
   });
 };
 
 function makeElements(tasks){
-
-console.log(tasks)
-
+  console.log(tasks)
 }
